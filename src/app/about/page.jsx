72852196d1a2616dcd3ed7b45e258cf5b9512a84
jakChi <kobachincharauli8@gmail.com>
2024-04-13 16:26:20 +0400

    fixed some buts

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
-import "../../styles/About.css"
+import "../../styles/About.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLightbulb, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
@@ -53,9 +53,9 @@ const Contact = () => {
         <div class="author">
           <p class="author-name">
             By
-            <a href="https://freecodecamp.org" target="_blank" rel="noreferrer"
-              >freeCodeCamp</a
-            >
+            <a href="https://freecodecamp.org" target="_blank" rel="noreferrer">
+              freeCodeCamp
+            </a>
           </p>
           <p class="publish-date">March 7, 2019</p>
         </div>
@@ -79,14 +79,27 @@ const Contact = () => {
       </section>
       <section class="text">
         <p class="first-paragraph">
-          Soon the freeCodeCamp curriculum will be 100% project-driven learning. Instead of a series of coding challenges, you'll learn through building projects - step by step. Before we get into the details, let me emphasize: we are not changing the certifications. All 6 certifications will still have the same 5 required projects. We are only changing the optional coding challenges.
+          Soon the freeCodeCamp curriculum will be 100% project-driven learning.
+          Instead of a series of coding challenges, ll learn through
+          building projects - step by step. Before we get into the details, let
+          me emphasize: we are not changing the certifications. All 6
+          certifications will still have the same 5 required projects. We are
+          only changing the optional coding challenges.
         </p>
         <p>
-          After years - years - of pondering these two problems and how to solve them, I slipped, hit my head on the sink, and when I came to I had a revelation! A vision! A picture in my head! A picture of this! This is what makes time travel possible: the flux capacitor!
+          After years - years - of pondering these two problems and how to solve
+          them, I slipped, hit my head on the sink, and when I came to I had a
+          revelation! A vision! A picture in my head! A picture of this! This is
+          what makes time travel possible: the flux capacitor!
         </p>
         <p>
-          It as dramatic arevelation in Back to the Future. It
-          just occurred to me while I was going for a run. The revelation: the entire curriculum should be a series of projects. Instead of individual coding challenges, we'll just have projects, each with their own seamless series of tests. Each test gives you just enough information to figure out how to get it to pass. (And you can view hints if that isn't enough.)
+          It as dramatic arevelation in Back to the Future. It just occurred to
+          me while I was going for a run. The revelation: the entire curriculum
+          should be a series of projects. Instead of individual coding
+          challenges,  just have projects, each with their own seamless
+          series of tests. Each test gives you just enough information to figure
+          out how to get it to pass. (And you can view hints if that
+          enough.)
         </p>
         <blockquote>
           <hr />
@@ -97,13 +110,28 @@ const Contact = () => {
         </blockquote>
         <p>
           No more walls of explanatory text. No more walls of tests. Just one
-          test at a time, as you build up a working project. Over the course of passing thousands of tests, you build up projects and your own understanding of coding fundamentals. There is no transition between lessons and projects, because the lessons themselves are baked into projects. And there's plenty of repetition to help you retain everything because - hey - building projects in real life has plenty of repetition.
+          test at a time, as you build up a working project. Over the course of
+          passing thousands of tests, you build up projects and your own
+          understanding of coding fundamentals. There is no transition between
+          lessons and projects, because the lessons themselves are baked into
+          projects. And  plenty of repetition to help you retain
+          everything because - hey - building projects in real life has plenty
+          of repetition.
         </p>
         <p>
-          The main design challenge is taking what is currently paragraphs of explanation and instructions and packing them into a single test description text. Each project will involve dozens of tests like this. People will be coding the entire time, rather than switching back and forth from "reading mode" to "coding mode".
+          The main design challenge is taking what is currently paragraphs of
+          explanation and instructions and packing them into a single test
+          description text. Each project will involve dozens of tests like this.
+          People will be coding the entire time, rather than switching back and
+          forth from
         </p>
         <p>
-          Instead of a series of coding challenges, people will be in their code editor passing one test after another, quickly building up a project. People will get into a real flow state, similar to what they experience when they build the required projects at the end of each certification. They'll get that sense of forward progress right from the beginning. And freeCodeCamp will be a much smoother experience.
+          Instead of a series of coding challenges, people will be in their code
+          editor passing one test after another, quickly building up a project.
+          People will get into a real flow state, similar to what they
+          experience when they build the required projects at the end of each
+          get that sense of forward progress right from
+          the beginning. And freeCodeCamp will be a much smoother experience.
         </p>
       </section>
       <section class="text text-with-images">
@@ -115,7 +143,7 @@ const Contact = () => {
               <h4 class="list-subtitle">V1 - 2014</h4>
               <p>
                 We launched freeCodeCamp with a simple list of 15 resources,
-                including  CS50 and  Database Class.
+                including CS50 and Database Class.
               </p>
             </li>
             <li>
@@ -126,7 +154,7 @@ const Contact = () => {
               <h4 class="list-subtitle">V3 - 2015</h4>
               <p>
                 We added our own HTML+CSS challenges (beforeeen relying on
-                General  Dash course for these).
+                General Dash course for these).
               </p>
             </li>
             <li>
@@ -146,8 +174,8 @@ const Contact = () => {
             <li>
               <h4 class="list-subtitle">V6 - 2018</h4>
               <p>
-                We launched 6 new certifications to replace our old ones. This was
-                the biggest curriculum improvement to date.
+                We launched 6 new certifications to replace our old ones. This
+                was the biggest curriculum improvement to date.
               </p>
             </li>
           </ul>
@@ -156,9 +184,9 @@ const Contact = () => {
           <blockquote class="image-quote">
             <hr />
             <p class="quote">
-              The millions of people who are learning to code through freeCodeCamp
-              will have an even better resource to help them learn these
-              fundamentals.
+              The millions of people who are learning to code through
+              freeCodeCamp will have an even better resource to help them learn
+              these fundamentals.
             </p>
             <hr />
           </blockquote>
